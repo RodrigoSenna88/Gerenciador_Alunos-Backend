83b189ad51e3aa2fb28673b4c0bdba9df8e00b8d
Rodrigo Senna <rodrigosennasantos@hotmail.com>
2020-05-15 18:03:29 -0300

    configurando eslint e editor config

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,8 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({message: 'Hello Goooo'});
-})
+app.get('/', (request, response) => response.json({ message: 'Hello Goooo' }));
 
 app.listen(3333, () => {
   console.log('servidor online');
 });
-
