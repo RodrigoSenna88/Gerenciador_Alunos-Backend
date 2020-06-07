3169e11aa86296e17379f0c5ab092ce1b02b5feb
Rodrigo Senna <rodrigosennasantos@hotmail.com>
2020-06-07 20:37:58 -0300

    criando a migration registers

---
+++ src/database/migrations/1591561104186-CreateRegisters.ts
@@ -0,0 +1,50 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export default class CreateRegisters1591561104186 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.createTable(
+        new Table({
+            name: 'registers',
+            columns: [
+              {
+                name: 'id',
+                type: 'varchar',
+                isPrimary: true,
+                generationStrategy: 'uuid',
+              },
+              {
+                name: 'name',
+                type: 'varchar',
+                isNullable: false,
+              },
+              {
+                name: 'phone',
+                type: 'integer',
+                isNullable: false,
+              },
+              {
+                name: 'responsable',
+                type: 'varchar',
+                isNullable: false,
+              },
+              {
+                name: 'startDate',
+                type: 'timestamp with time zone',
+                isNullable: false,
+              },
+              {
+                name: 'schedule',
+                type: 'varchar',
+                isNullable: false,
+              },
+            ]
+        })
+      );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.dropTable('registers')
+    }
+
+}
