0c24a192bcbe4990b11cf0e7c08f62ede6fcfc09
Rodrigo Senna <rodrigosennasantos@hotmail.com>
2020-06-12 19:09:18 -0300

    configurada as verificações das credenciais (email e senha)

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import registersRouter from '../routes/registers.routes';
 import usersRouter from '../routes/users.routes';
+import sessionsRouter from '../routes/sessions.routes';
 
 const routes = Router();
 
 routes.use('/registers', registersRouter);
 routes.use('/users', usersRouter);
+routes.use('/sessions', sessionsRouter);
 
 export default routes;
