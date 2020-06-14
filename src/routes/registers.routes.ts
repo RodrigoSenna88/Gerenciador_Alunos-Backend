f02a82066c9961b0cb36cb2fa53f83864a4faa13
Rodrigo Senna <rodrigosennasantos@hotmail.com>
2020-06-14 15:03:22 -0300

    configurando upload de arquivos (que ainda estão sendo salvos na máquina e não no banco de dados). Correção de erro de digitação no plugin do prettier.

--- src/routes/registers.routes.ts
+++ src/routes/registers.routes.ts
@@ -1,13 +1,13 @@
 import { Router } from 'express';
-import{ getCustomRepository } from 'typeorm';
-import { parseISO } from 'date-fns'
+import { getCustomRepository } from 'typeorm';
+import { parseISO } from 'date-fns';
 
 import RegistersRepository from '../repositories/RegistersRepository';
 import CreateRegisterService from '../services/CreateRegisterService';
 
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
-//DTO - Data Transfer Object
+// DTO - Data Transfer Object
 
 const registersRouter = Router();
 
@@ -18,25 +18,37 @@ registersRouter.get('/', async (request, response) => {
   const registers = await registersRepository.find();
 
   return response.json(registers);
-})
+});
 // POST http://localhost:3333/registers
 
 registersRouter.post('/', async (request, response) => {
-try{
-const { manager, student, phone, responsible, startDate, schedule } = request.body;
+  try {
+    const {
+      manager,
+      student,
+      phone,
+      responsible,
+      startDate,
+      schedule,
+    } = request.body;
 
-const parsedDate = parseISO(startDate);
+    const parsedDate = parseISO(startDate);
 
-const createRegister = new CreateRegisterService();
+    const createRegister = new CreateRegisterService();
 
-const register = await createRegister.execute({ manager, student, phone, responsible, startDate: parsedDate, schedule})
+    const register = await createRegister.execute({
+      manager,
+      student,
+      phone,
+      responsible,
+      startDate: parsedDate,
+      schedule,
+    });
 
-return response.json(register);
-} catch (err) {
-  return response.status(400).json({ error: err.message });
-}
-
+    return response.json(register);
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
 });
-
 
 export default registersRouter;
