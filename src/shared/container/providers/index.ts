62994dd03dfedd7cfc4107e20ee5eb5f7326e2a0
Rodrigo Senna <rodrigosennasantos@hotmail.com>
2020-06-30 15:16:19 -0300

    criando branch 'perfil-usuario'. > Iniciando a configuração de teste para recuperação de senha. (SendForgotPasswordEmailService.spec.ts).

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -3,7 +3,15 @@ import { container } from 'tsyringe';
 import IStorageProvider from './StorageProvider/models/IStorageProvider';
 import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
 
+// import IMailProvider from './MailProvider/models/IMailProvider';
+// import DiskStorageProvider from './MailProvider/implementations/DiskStorageProvider';
+
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
   DiskStorageProvider,
-)
+);
+
+// container.registerSingleton<IStorageProvider>(
+//   'StorageProvider',
+//   DiskStorageProvider,
+// );
