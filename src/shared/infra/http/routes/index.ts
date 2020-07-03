42a853bc3da47caa83df055d29298acc498e60e8
Rodrigo Senna <rodrigosennasantos@hotmail.com>
2020-07-03 11:32:31 -0300

    Configurando envio de email em desenvolvimento utilizando Ethereal Mail

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -9,6 +9,6 @@ const routes = Router();
 routes.use('/registers', registersRouter);
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
-routes.use('/sessions', passwordRouter);
+routes.use('/password', passwordRouter);
 
 export default routes;
