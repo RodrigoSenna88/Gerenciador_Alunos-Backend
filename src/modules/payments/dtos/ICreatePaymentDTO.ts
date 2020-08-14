0fb761e72dd80f4c32bde1f9bd01a9bcf7051f93
Rodrigo Senna <rodrigosennasantos@hotmail.com>
2020-08-14 19:42:31 -0300

    criando DTO de payment e IPaymentRepository

---
+++ src/modules/payments/dtos/ICreatePaymentDTO.ts
@@ -0,0 +1,6 @@
+export default interface ICreatePaymentDTO {
+  student: string;
+  responsible: string;
+  month: string;
+  payment: boolean;
+}
---
+++ src/modules/payments/repositories/IPaymentRepository.ts
@@ -0,0 +1,6 @@
+import Payment from '@modules/payments/infra/typeorm/entities/Payment';
+import ICreatePaymentDTO from '@modules/payments/dtos/ICreatePaymentDTO';
+
+export default interface IPaymentRepository {
+  payment(data: ICreatePaymentDTO): Promise<Payment>;
+}
