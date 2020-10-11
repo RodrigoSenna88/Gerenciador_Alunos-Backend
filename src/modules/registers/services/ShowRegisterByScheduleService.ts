21171d67191b3a98abde52c5c900a1cc91a9cf47
Rodrigo Senna <rodrigosennasantos@hotmail.com>
2020-10-11 15:54:32 -0300

    acertando branch

--- src/modules/registers/services/ShowRegisterByScheduleService.ts
+++ src/modules/registers/services/ShowRegisterByScheduleService.ts
@@ -2,6 +2,7 @@ import { injectable, inject } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
 
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 import Register from '../infra/typeorm/entities/Register';
 import IRegisterRepository from '../repositories/IRegistersRepository';
 
@@ -14,20 +15,38 @@ class ShowRegisterByScheduleService {
   constructor(
     @inject('RegistersRepository')
     private registersRepository: IRegisterRepository,
+
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
   ) {}
 
   public async execute({ schedule }: IRequest): Promise<Register[]> {
-    const registerSchedule = schedule;
-    const register = await this.registersRepository.findRegisterBySchedule(
-      registerSchedule,
+    let register = await this.cacheProvider.recover<Register[]>(
+      `registerSchedule-list:${schedule}`,
     );
 
     if (!register) {
-      throw new AppError('This schedule have no one registred.');
+      const registerSchedule = schedule;
+      register = await this.registersRepository.findRegisterBySchedule(
+        registerSchedule,
+      );
+
+      if (!register) {
+        throw new AppError('This schedule have no one registred.');
+      }
     }
+
+    console.log('A query no banco foi feita');
+
+    await this.cacheProvider.save(
+      `registerSchedule-list:${schedule}`,
+      register,
+    );
+
 
     return register;
   }
+
 }
 
 export default ShowRegisterByScheduleService;
